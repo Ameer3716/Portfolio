5326dd35cdde38f1fb50be522ba34d2061084a46
Ameer3716 <f223716@cfd.nu.edu.pk>
2025-02-02 17:39:02 +0500

    Obj

--- jsfolder/obj.js
+++ jsfolder/obj.js
@@ -76,3 +76,39 @@ createPerson.prototype.greet = function () {
 // Creating an instance and testing
 let person3 = new createPerson("Ali", true);
 console.log(person3.greet()); // Output: Hello, Ali
+
+
+var collection={
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+};
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+function updateRecords(id, prop, value) {
+  if(value === ""){
+    delete collection[id][prop];
+  }else if(prop === "tracks"){
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value);
+  }else{
+    collection[id][prop] = value;
+  }
+  return collection;
+}
